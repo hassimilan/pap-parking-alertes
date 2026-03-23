2983563cf19322b2010ad921b5048a591ce00a86
hassimilan <hassimilan@yahoo.fr>
2026-03-23 11:04:19 +0100

    Update pap_surfaces_diverses.py

--- pap_surfaces_diverses.py
+++ pap_surfaces_diverses.py
@@ -99,7 +99,7 @@ def main():
     log.info("Demarrage Surfaces IDF")
     telegram("Surfaces IDF demarre - 3 pages - max "+str(PRIX_MAX)+" EUR - toutes les 5 min")
     vues=charger()
-    premiere=len(vues)==0
+    premiere=False
     while True:
         try:
             nouvelles=[]
